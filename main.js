9aa5e9bc3850e2fcd793f9133bf24496bcebebf7
Noparat Prasongdee <noparat.saimai@mail.kmutt.ac.th>
2021-02-05 13:45:38 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+    const app = {
+        data() {
+            return {
+                msg: 'Hello, Vue3'
+            }
+        }
+
+    }
+=======
+    const app = {
+        data() {
+            return {
+                msg: 'Hello, Vue3'
+            }
+        }
+
+    }
+>>>>>>> new
+    Vue.createApp(app).mount('#app')
